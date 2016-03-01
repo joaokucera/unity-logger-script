f9fb6bb124800666c31b595a1386a3a3480974cf
joaokucera <falleirok@gmail.com>
2016-03-01 15:18:13 -0300

    Logger Unity C#
    A logger C# script to be used with unity.

---
+++ Logger.cs
@@ -0,0 +1,102 @@
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+/// <summary>
+/// A logger script that wraps Unity's internal logger.
+/// Calls to its methods are stripped in case the LOGGER_SYMBOL is not defined.
+/// </summary>
+public sealed class Logger
+{
+	public const string LOGGER_SYMBOL = "ENABLE_LOG";
+
+	[Conditional(LOGGER_SYMBOL)]
+	public static void Log(object message)
+	{
+		Debug.Log(message);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void Log(object message, Object context)
+	{
+		Debug.Log(message, context);
+	}
+
+#if UNITY_5_0
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogFormat(string message, params object[] args)
+	{
+		Debug.LogFormat(message, args);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogFormat(Object context, string message, params object[] args)
+	{
+		Debug.LogFormat(context, message, args);
+	}
+#endif
+
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogWarning(object message)
+	{
+		Debug.LogWarning(message);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogWarning(object message, Object context)
+	{
+		Debug.LogWarning(message, context);
+	}
+
+#if UNITY_5_0
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogWarningFormat(string message, params object[] args)
+	{
+		Debug.LogWarningFormat(message, args);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogWarningFormat(Object context, string message, params object[] args)
+	{
+		Debug.LogWarningFormat(context, message, args);
+	}
+#endif
+
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogError(object message)
+	{
+		Debug.LogError(message);
+	}
+
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogError(object message, Object context)
+	{
+		Debug.LogError(message, context);
+	}
+
+#if UNITY_5_0
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogErrorFormat(string message, params object[] args)
+	{
+		Debug.LogErrorFormat(message, args);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogErrorFormat(Object context, string message, params object[] args)
+	{
+		Debug.LogErrorFormat(context, message, args);
+	}
+#endif
+
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogException(System.Exception exception)
+	{
+		Debug.LogException(exception);
+	}
+	
+	[Conditional(LOGGER_SYMBOL)]
+	public static void LogException(System.Exception exception, Object context)
+	{
+		Debug.LogException(exception, context);
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# A logger C# script to be used with unity #
+
+This repository has a script to use log and do not create junk in your game.
+
+### Change Log ###
+
+1.0.0
